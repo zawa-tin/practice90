9aec540b568a8fac3517053f31e19dc5076e26be
zawakasutera <zawazawanomorichan@outlook.jp>
2022-04-02 16:19:24 +0900

    033

---
+++ 033.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+// 027
+// https://atcoder.jp/contests/typical90/tasks/typical90_aa
+
+// clang-format off
+
+using namespace std;
+#define all(a) a.begin(), a.end()
+#define int long long
+#define over_load_(_1, _2, _3, _4, NAME, ...) NAME
+#define rep(...) over_load_(__VA_ARGS__, rep4, rep3, rep2)(__VA_ARGS__)
+#define rep2(i, r) for (int i = 0; i < static_cast<int>(r); (i) += 1)
+#define rep3(i, l, r) for (int i = static_cast<int>(l); i < static_cast<int>(r); (i) += 1)
+#define rep4(i, l, r, stride) for (int i = static_cast<int>(l); i < static_cast<int>(r); (i) += (stride))
+#define rrep(...) over_load_(__VA_ARGS__, rrep4, rrep3, rrep2)(__VA_ARGS__)
+#define rrep2(i, r) for (int i = static_cast<int>(r) - 1; i >= 0; (i) -= 1)
+#define rrep3(i, l, r) for (int i = static_cast<int>(r) - 1; i >= static_cast<int>(l); (i) -= 1)
+#define rrep4(i, l, r, stride) for (int i = static_cast<int>(r) - 1; i >= static_cast<int>(l); (i) -= (stride))
+#define fore(i, a) for (auto &i : a)
+template <typename T1, typename T2>
+inline bool chmax(T1 &a, T2 b)
+{
+    return a < b && (a = b, true);
+}
+template <typename T1, typename T2>
+inline bool chmin(T1 &a, T2 b)
+{
+    return a > b and (a = b, true);
+}
+constexpr char newl = '\n';
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+using vp = vector<pair<int, int>>;
+using vs = vector<string>;
+
+int div(int x) {
+    return (x + 1) / 2;
+}
+
+signed main() {
+    int h, w; cin >> h >> w;
+    if (h == 1 or w == 1) {
+        cout << h * w << newl;
+        return 0;
+    }
+    cout << div(h) * div(w) << newl;
+}
+
+/*
+    自分の解法
+    全探索して貪欲にLEDを置いていく-> WA
+    2の切り上げ -> WA
+    解説AC
+    「完全に含まれる」なのでH = 1 or W = 1の時は全マスにライトをおける
+*/
